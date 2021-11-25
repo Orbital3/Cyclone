8bdff1d76df75b4687d61a0f84eae13881346f55
Raphaël Fillion <raph.fillion18@gmail.com>
2021-11-25 10:46:31 -0500

    added animation

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import '/style.scss';
 
+let dotYellow = document.querySelector('.dot.yellow');
+let dots = document.querySelectorAll('.dot');
+
+
+for(let i = 0; i < dots.length; i++) {
+    setTimeout(function timer() {
+        dots[i].classList.add('yellow');
+        setTimeout(() => {
+            dots[i].classList.remove('yellow');
+        }, 500)
+    }, i * 500)
+}
+
 
