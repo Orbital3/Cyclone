3d81819200fe0528eca33850acea092b3ac3903e
Raphaël Fillion <raph.fillion18@gmail.com>
2021-12-02 10:30:34 -0500

    arduino

---
+++ 2_arduino/cyclone/cyclone.ino
@@ -0,0 +1,223 @@
+// Viral Science www.viralsciencecreativity.com www.youtube.com/c/viralscience
+
+#include "FastLED.h"  //FastLED.h Library Download
+#define NUM_LEDS 63  //Total Number of LED
+#define DATA_PIN 5   //Cyclone LED Strip D2
+#define SCORE_PIN 6  //Level LED Strip D6
+#define SCORE_LEDS 4
+
+CRGB leds[NUM_LEDS];
+CRGB sleds[NUM_LEDS];
+
+bool reachedEnd = false;
+byte gameState = 0;
+//byte ledSpeed = 0;
+int period = 1000;
+unsigned long time_now = 0;
+byte Position = 0;
+byte level = 0;
+
+int point = 0;
+
+const byte ledSpeed[4] = {50, 40, 35, 20};
+
+//Debounce
+bool findRandom = false;
+byte spot = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, SCORE_PIN, RGB>(sleds, SCORE_LEDS);
+  pinMode(4, INPUT_PULLUP);                                           //Push Button D4
+  Serial.begin(38400);
+  Serial.println("Reset");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (gameState == 0) {
+    fill_rainbow(leds, NUM_LEDS, 0, 4); //2 = longer gradient strip
+    fill_rainbow(sleds, SCORE_LEDS, 0, 7); //2 = longer gradient strip
+    if (digitalRead(4) == LOW) {
+      Position = 0;
+      findRandom = true;
+      delay(500);
+      for (byte i = 0; i < NUM_LEDS; i++) {
+        leds[i].setRGB(0, 0, 0);
+        delay(40);
+        FastLED.show();
+      }
+      for (byte i = 0; i < SCORE_LEDS; i++) {
+        sleds[i].setRGB(0, 0, 0);
+        delay(40);
+        FastLED.show();
+      }
+      gameState = 1;
+    }
+    FastLED.show();
+  }
+  if (gameState == 1) {
+    period = ledSpeed[0];
+    if (millis() > time_now + period) {
+      time_now = millis();
+      if (findRandom) {
+        spot = random(NUM_LEDS - 1);              //Total Cyclone LED - 4
+        findRandom = false;
+      }
+      leds[spot - 1].setRGB(255, 140, 0);
+      leds[spot].setRGB(0, 255, 0);
+      leds[spot + 1].setRGB(255, 140, 0);
+      sleds[0].setRGB(0, 0, 255);
+      PlayGame(spot-1, spot+1);
+    }
+    if (digitalRead(4) == LOW) {
+      delay(300);
+      findRandom = false;
+      if (Position > spot - 1 && Position < spot + 3) {
+        point++;
+        level = gameState;
+        gameState = 98;
+      } else {
+        gameState = 99;
+      }
+    }
+  }
+  if (gameState == 2) {
+//    period = 320;
+    period = ledSpeed[1];
+    if (millis() > time_now + period) {
+      time_now = millis();
+      if (findRandom) { 
+        spot = random(NUM_LEDS - 1);              //Total Cyclone LED - 4
+        findRandom = false;
+      }
+      leds[spot - 1].setRGB(255, 140, 0);
+      leds[spot].setRGB(0, 255, 0);
+      leds[spot + 1].setRGB(255, 140, 0);
+      sleds[1].setRGB(0, 0, 255);
+      PlayGame(spot-1, spot+1);
+    }
+    if (digitalRead(4) == LOW) {
+      delay(300);
+      if (spot - 1 && Position < spot + 3) {
+        point++;
+        level = gameState;
+        gameState = 98;
+      } else {
+        gameState = 99;
+      }
+    }
+  }
+  if (gameState == 3) {
+    period = ledSpeed[2];
+    if (millis() > time_now + period) {
+      time_now = millis();
+      if (findRandom) {
+        spot = random(NUM_LEDS - 1);              //Total Cyclone LED - 4
+        findRandom = false;
+      }
+      leds[spot].setRGB(0, 255, 0);
+      sleds[2].setRGB(0, 0, 255);
+      PlayGame(spot, spot);
+    }
+    if (digitalRead(4) == LOW) {
+      delay(300);
+      if (Position == spot+1) {
+        point++;
+        level = gameState;
+        gameState = 98;
+      } else {
+        gameState = 99;
+      }
+    }
+  }
+  if (gameState == 4) {
+    period = ledSpeed[3];
+    if (millis() > time_now + period) {
+      time_now = millis();
+      if (findRandom) {
+        spot = random(NUM_LEDS - 1);              //Total Cyclone LED - 4
+        findRandom = false;
+      }
+      leds[spot].setRGB(0, 255, 0);
+      sleds[3].setRGB(0, 0, 255);
+      PlayGame(spot, spot);
+    }
+    if (digitalRead(4) == LOW) {
+      delay(300);
+      if (Position == spot+1) {
+        point++;
+        level = gameState;
+        gameState = 98;
+      } else {
+        gameState = 99;
+      }
+    }
+  }
+  if (gameState == 98) {
+    winner();
+  }
+  if (gameState == 99) {
+    loser();
+  }
+}
+void PlayGame(byte bound1, byte bound2) {
+  leds[Position].setRGB(255, 0, 0);
+  if (Position < bound1 + 1 || Position > bound2 + 1) {
+    leds[Position - 1].setRGB(0, 0, 0);
+  }
+  FastLED.show();
+  Position++;
+  if (Position >= NUM_LEDS) {
+    leds[Position - 1].setRGB(0, 0, 0);
+    Position = 0;
+  }
+  Serial.print("/pos ");
+  Serial.println(Position);
+}
+
+void winner() {
+  Serial.print("/poi ");
+  Serial.println(point);
+  
+  for (byte i = 0; i < 3; i++) {
+    for (byte j = 0; j < NUM_LEDS; j++) {
+      leds[j].setRGB(0, 255, 0);
+    }
+    FastLED.show();
+    delay(500);
+    clearLEDS();
+    FastLED.show();
+    delay(500);
+  }
+  findRandom = true;
+  Position = 0;
+
+  gameState = level + 1;
+  if (gameState > 4) {
+    gameState = 0;
+  }
+}
+void loser() {
+  point = 0;
+  for (byte i = 0; i < 3; i++) {
+    for (byte j = 0; j < NUM_LEDS; j++) {
+      leds[j].setRGB(255, 0, 0);
+    }
+    FastLED.show();
+    delay(500);
+    clearLEDS();
+    FastLED.show();
+    delay(500);
+  }
+  gameState = 0;
+}
+void clearLEDS() {
+  for (byte i = 0; i < NUM_LEDS; i++) {
+    leds[i].setRGB(0, 0, 0);
+  }
+}
+void winAll(){
+  
+}
